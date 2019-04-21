53613573f4287e616a38fba016e8dd8913fc5566
Umar Abbas <omer.ithj@gmail.com>
2019-04-21 13:42:30 +0400

    Add project files.

---
+++ TyslinExAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TyslinExAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
